af73d384ced327b23c494fe2cf90a8a287aa2847
Cassandra <mussardcassandra@gmail.com>
2021-12-08 15:50:47 +0100

    t

--- Huffman.ads
+++ Huffman.ads
@@ -1,3 +1,22 @@
-Type T_Tableau is array(1..128) of integer;
+generic
+type T_Donnee is private;
+    type T_Cle is private;
 
-procedure Calcul_frequence(Tableau : out T_Tableau);
+package TH is
+    type T_Tableau is limited private;
+
+
+
+private
+  type T_Tableau is array(0..128) of T_LCA;
+        type T_Cellule is
+        		record
+        			Indice : Integer;
+        			Valeur : Float;
+        			Suivant : T_Vecteur_Creux;
+        			-- Invariant :
+        			--   Indice >= 1;
+        			--   Suivant = Null or else Suivant.all.Indice > Indice;
+        			--   	-- les cellules sont stockés dans l'ordre croissant des indices.
+        		end record;
+          end TH;
