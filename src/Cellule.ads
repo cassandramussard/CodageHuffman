fb4567a4895d99645c0290d3f35d2e3999f58d79
Baptiste <baptiste.cruvellier31@gmail.com>
2021-12-10 16:09:57 +0100

    Cellule.ads / adb / exeption

--- src/Cellule.ads
+++ src/Cellule.ads
@@ -1,39 +1,59 @@
+generic
+   type T_Cle is private;
+   type T_Donnee is private;
+
 package Cellule is
    type T_Cellule is limited private;
 
    -- Initialiser la cellule
    procedure Initialiser(Cellule : out T_Cellule);
 
+   procedure Vider(Cellule : in out T_Cellule) with
+     Post => Est_Vide (Cellule);
+
    -- La Cellule est vide
-   function Est_Vide(Cellule : out T_Cellule) return Boolean;
+   function Est_Vide(Cellule : in T_Cellule) return Boolean;
+
+   -- Obtenir le nombre d'éléments d'une Cellule.
+   function Taille (Cellule : in T_Cellule) return Integer with
+     Post => Taille'Result >= 0
+     and (Taille'Result = 0) = Est_Vide (Cellule);
 
    --Vérifier si la cellule est une feuille
    function Est_Feuille(Cellule : in T_Cellule) return Boolean;
 
    -- Vérifier si le carctere est présent dans la cellule
-   procedure Caractere_Present(Cellule : in T_Cellule ; Caractere : in Character);
+   function Cle_Presente(Cellule : in T_Cellule ; Cle : in T_Cle) return Boolean;
+
+   function La_Donnee (Cellule : in T_Cellule ; Cle : in T_Cle) return T_Donnee;
+
 
    -- Enregistrer une cellule d'une fiche avec ses fils droit et gauche, sa frequence et le caractere
-   procedure Enregistrer(Cellule : in out T_Cellule ; Caractere : in Character ; Frequence : in Integer; Fils_droit : in T_Cellule ; Fils_gauche : in T_Cellule);
+   procedure Enregistrer(Cellule : in out T_Cellule ; Cle : in T_Cle ; Donnee : in T_Donnee; Fils_droit : in T_Cellule ; Fils_gauche : in T_Cellule);
 
-   procedure Supprimer (Cellule : in out T_Cellule ; Caractere : in Character);
-
-   function Donne_Frequence(Cellule : in T_Cellule ; Caractere : in Character) return Integer;
+   -- Supprimer la Donn�e associ�e � une Cl� dans une Cellule.
+   -- Exception : Cle_Absente_Exception si Cl� n'est pas utilis�e dans la Cellule
+   procedure Supprimer(Cellule : in out T_Cellule ; Cle : in T_Cle) with
+     Post =>  Taille (Cellule) = Taille (Cellule)'Old - 1 -- un �l�ment de moins
+     and not Cle_Presente (Cellule, Cle);
 
    -- generique avec fonction traiter
+   generic
+      with procedure Traiter (Cle : in T_Cle; Donnee: in T_Donnee);
    procedure Parcrour_Infixe(Cellule : in out T_Cellule);
 
 
-   --
 
 private
    type T_Element;
+
    type T_Cellule is access T_Element;
+
    type T_Element is
       record
+         Donnee : T_Donnee;
+         Cle : T_Cle;
          Fils_gauche : T_Cellule;
          Fils_droit : T_Cellule;
-         Frequence : Integer;
-         Caractere : Character;
       end record;
 end Cellule;
--- src/codagehuffman.ads
+++ src/codagehuffman.ads
@@ -1,17 +1,16 @@
-
 with Cellule;
 
 package Huffman is
   type T_Tableau is array(0..128) of Integer;
 
 -- Calculer les fréquences des caractères du texte
-  function Calcul_Frequence(texte : in String) return T_Tableau
+  function Calcul_Frequence(texte : in String) return T_Tableau;
 
 -- Construire l'arbre de Huffman grâce aux fréquences des caractères
   function Construire_Arbre(Tableau : in T_Tableau) return T_Cellule;
 
 -- Afficher l'arbre de Huffman
-  procedure Afficher_Arbre(Cellule : in T_Cellule) with
+  procedure Afficher_Arbre(Cellule : in T_Cellule);
 
 -- Compresser le fichier
   function Compresser_ficher(texte : in String) return String;
@@ -21,6 +20,6 @@ package Huffman is
 
 private
   package Cellule_Huffman is
-    new Cellule(Fils_gauche => T_Cellule, Fils_droit => T_Cellule, Frequence => Integer, Caractere => Character)
+    new Cellule(Fils_gauche => T_Cellule, Fils_droit => T_Cellule, Frequence => Integer, Caractere => Character);
     use Cellule_Huffman;
 end Huffman;
